09b22329fa4912363b5aded1bcd552da6680dc30
Mohamad <mkb31@mail.aub.edu>
2024-11-13 19:38:27 +0200

    Deleted Moune

--- Moune/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///moune_ecommerce.db'
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
